e9dbfa992a8c973bd8b69c572b69c0ace39c7a8e
Vetrivelmaran <vetri4723@gmai.com>
2023-08-31 11:31:56 +0530

    first commit

---
+++ capitalize_name.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array of objects with a name property and returns a new
+// array of strings with the name of each object capitalized. Use the Map object to
+// accomplish this.
+let a=[{name:'vetri',age:23},{name:'david',age:34}]
+let r=a.map(e=>{
+    a[0]=a[0].upp
+})
